f01b9838083780691e979c0007b518f014716e60
Anca6889 <guth.jeanmaxime@gmail.com>
2021-01-14 15:35:36 +0100

    Commit 3

--- config/config.py
+++ config/config.py
@@ -1,10 +1,11 @@
+""" This module contains all the constants of the program """
+
 #!/usr/bin/python3
 # -*- coding: Utf-8 -*
 
-""" This file contains all the constants of the program """
-
 # selected categories for the local data base
-CATEGORIES = ["pates-a-tartiner", "boissons", "condiments", "biscuits-et-gateaux", "pizzas"]
+CATEGORIES = ["pates-a-tartiner", "boissons", "condiments", 
+"biscuits-et-gateaux", "pizzas"]
 
 # Maximal number of product by categorie
 P_MAX = 50
