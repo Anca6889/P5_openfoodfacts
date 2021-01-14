2ec2b3747b64e04c56d5a5bac50466e19901f62c
Anca6889 <guth.jeanmaxime@gmail.com>
2021-01-14 12:24:50 +0100

    1st commit

---
+++ config/config.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# -*- coding: Utf-8 -*
+
+""" This file contains all the constants of the program """
+
+# selected categories for the local data base
+CATEGORIES = ["pates-a-tartiner", "boissons", "condiments", "biscuits-et-gateaux", "pizzas"]
+
+# Maximal number of product by categorie
+P_MAX = 50
---
+++ data_base/db_p5 (1).sql
@@ -0,0 +1,108 @@
+CREATE DATABASE  IF NOT EXISTS `db_p5` /*!40100 DEFAULT CHARACTER SET utf8 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `db_p5`;
+-- MySQL dump 10.13  Distrib 8.0.22, for Win64 (x86_64)
+--
+-- Host: localhost    Database: db_p5
+-- ------------------------------------------------------
+-- Server version	8.0.22
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `category`
+--
+
+DROP TABLE IF EXISTS `category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `category` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(200) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `category`
+--
+
+LOCK TABLES `category` WRITE;
+/*!40000 ALTER TABLE `category` DISABLE KEYS */;
+/*!40000 ALTER TABLE `category` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product`
+--
+
+DROP TABLE IF EXISTS `product`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `product` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `Category_id` int NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `nutriscore_grade` char(1) NOT NULL,
+  `store` varchar(200) NOT NULL,
+  `url` varchar(1000) NOT NULL,
+  PRIMARY KEY (`id`,`Category_id`),
+  KEY `fk_Product_Category1_idx` (`Category_id`),
+  CONSTRAINT `fk_Product_Category1` FOREIGN KEY (`Category_id`) REFERENCES `category` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product`
+--
+
+LOCK TABLES `product` WRITE;
+/*!40000 ALTER TABLE `product` DISABLE KEYS */;
+/*!40000 ALTER TABLE `product` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `substitution`
+--
+
+DROP TABLE IF EXISTS `substitution`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `substitution` (
+  `Product_id` int NOT NULL,
+  `Substitute_Name` int NOT NULL,
+  PRIMARY KEY (`Product_id`,`Substitute_Name`),
+  KEY `fk_Product_has_Product_Product1_idx` (`Substitute_Name`),
+  KEY `fk_Product_has_Product_Product_idx` (`Product_id`),
+  CONSTRAINT `fk_Product_has_Product_Product` FOREIGN KEY (`Product_id`) REFERENCES `product` (`id`),
+  CONSTRAINT `fk_Product_has_Product_Product1` FOREIGN KEY (`Substitute_Name`) REFERENCES `product` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `substitution`
+--
+
+LOCK TABLES `substitution` WRITE;
+/*!40000 ALTER TABLE `substitution` DISABLE KEYS */;
+/*!40000 ALTER TABLE `substitution` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-01-13 17:31:20
